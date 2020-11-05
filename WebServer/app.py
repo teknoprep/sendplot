8e0e4f395b847c38f73aed105aabdf33517ce185
teknoprep <chris@blueuc.com>
2020-11-05 14:37:07 -0500

    updated .env

--- WebServer/app.py
+++ WebServer/app.py
@@ -19,7 +19,6 @@ path_url = {}
 url_path = {}
 
 app = Flask(__name__)
-app.run(host= '0.0.0.0')
 app.secret_key = 'l1wovKLN7xsMT5bieGN3vVnyhzQwNJmdmzzr5NMjC7AbaxJyRx34n5qXHuDBHBXUix2BLlBeoDZNh3XcNqUZLHC6oZzVioCq'
 
 
